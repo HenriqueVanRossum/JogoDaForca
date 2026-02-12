5ae8ca630a8ed6089785a6f3ff12225355701366
HenriqueVanRossum <henrique.rossum@outlook.com>
2026-02-11 21:39:05 -0300

    Jogo da forca com lista de palavras

---
+++ DesenharForca.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaForca
+{
+    public class DesenharForca
+    {
+        public void Desenhar(int erro)
+        {
+            switch (erro)
+            {
+
+                case 0:
+                    Console.Clear(); 
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    break;     
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine(" (x.x)");
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("(x.x)\n /");
+                    Console.WriteLine();
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("(x.x)\n /|");
+                    Console.WriteLine();
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.WriteLine("(x.x)\n /|\\");
+                    Console.WriteLine();
+                    break;
+                case 5:
+                    Console.Clear();
+                    Console.WriteLine("(x.x)\n /|\\\n  |");
+                    Console.WriteLine();
+                    break;
+                case 6:
+                    Console.Clear();
+                    Console.WriteLine("(x.x)\n /|\\\n  |\n /");
+                    Console.WriteLine();
+                    break;
+                case 7:
+                    Console.Clear();
+                    Console.WriteLine("(x.x)\n /|\\\n  |\n / \\");
+                    Console.WriteLine();
+                    break;
+
+                default:
+                    Console.WriteLine("Voce perdeu!");
+                    Environment.Exit(1); 
+                    break;
+            }
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿namespace JogoDaForca;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var listaLetrasErradas = new List<char>();
+        var listaLetras = new List<char>();
+        DesenharForca desenho = new DesenharForca();
+        string[] listaPalavrasSecretas =
+        {
+            "casa",
+            "bola",
+            "gato",
+            "livro",
+            "mesa",
+            "carro",
+            "flor",
+            "vento",
+            "peixe",
+            "nuvem",
+            "porta",
+            "amigo",
+            "jogo",
+            "tempo",
+            "papel"
+        };
+        Random numSorteio = new Random();
+        
+        string palavraSecreta = listaPalavrasSecretas[numSorteio.Next(listaPalavrasSecretas.Length -1)].ToUpper();
+        int erro = 0;
+        int control = 0;
+        foreach (var letra in palavraSecreta)
+        {
+            listaLetras.Add('_');
+            Console.Write(listaLetras[control]);
+            control++;
+        }
+        while (!palavraSecreta.ToUpper().Equals(string.Concat(listaLetras)))
+        {
+
+            Console.Write("Digite uma letra: ");
+            var palpiteLetra = char.Parse(Console.ReadLine()!.ToUpper());
+            desenho.Desenhar(erro);
+
+            if (!palavraSecreta.Contains(palpiteLetra))
+            {
+                erro++;
+                desenho.Desenhar(erro);
+                listaLetrasErradas.Add(palpiteLetra);
+            }
+            else
+            {
+                var controle = 0;
+                foreach (var letra in palavraSecreta)
+                {
+                    if (palpiteLetra == letra)
+                    {
+                        listaLetras[controle] = palpiteLetra;
+                    }
+                    controle++;
+                }
+            }
+            Console.WriteLine();
+
+            foreach (var letra in listaLetrasErradas)
+            {
+                Console.Write(letra + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach (var letra in listaLetras)
+            {
+                Console.Write(letra + " ");
+            }
+        }
+
+        Console.WriteLine("Voce ganhou!");
+
+    }
+}
